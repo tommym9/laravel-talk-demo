a887b069c3ef79129870106b1e97125f6c80d22f
Tom Metcalfe <tom.metcalf@reasondigital.com>
2018-02-06 20:06:51 +0000

    Added the comment relationship to the post model.

--- app/Post.php
+++ app/Post.php
@@ -5,6 +5,7 @@ namespace App;
 use Illuminate\Database\Eloquent\Model;
 use Cviebrock\EloquentSluggable\Sluggable;
 use Cviebrock\EloquentSluggable\SluggableScopeHelpers;
+use App\Comment;
 
 class Post extends Model
 {
@@ -23,5 +24,13 @@ class Post extends Model
                 'source' => 'title'
             ]
         ];
+    }
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function comments()
+    {
+        return $this->hasMany(Comment::class);
     }
 }
